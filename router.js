a44be13fcfc94990e34f4b1d17d5308d31809ac9
Pierpaolo <plucarelli.94@gmail.com>
2016-11-27 23:51:06 +0100

    removed dotenv file

--- router.js
+++ router.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-require('dotenv').config();
 var request = require('request');
 var weatherMap = require('./models/weatherMap.js');
 
