a211c0f59d4b0f565e78b460a7bd3cac04b8c061
Pierpaolo <plucarelli.94@gmail.com>
2016-12-01 17:41:25 +0100

    added CORS to express

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var router = require("./router.js");
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use(router);
 
 var server_port = process.env.PORT || 8080;
