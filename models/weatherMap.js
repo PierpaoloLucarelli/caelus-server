a78f1b165b28f1cc8be39f1aa24f0d64f51defd6
Pierpaolo <plucarelli.94@gmail.com>
2016-12-02 22:57:26 +0100

    Added collection for Fog

--- models/weatherMap.js
+++ models/weatherMap.js
@@ -2,6 +2,7 @@ var weatherMap = {
     Day: {
             Clear:          "394467",
             Atmosphere:     "411049",
+            Fog:            "411049",
             Snow:           "411052",
             Clouds:         "411046",
             Rain:           "428145",
@@ -11,6 +12,7 @@ var weatherMap = {
     Night: {
             Clear:          "411055",
             Atmosphere:     "428162",
+            Fog:            "428162",
             Snow:           "428149",
             Clouds:         "428144",
             Rain:           "428147",
